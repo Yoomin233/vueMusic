b27a2b569d7c032f781001dcbbfdcea9d6650427
Yuemin.Hu <yuemin.hu@baidao.com>
2017-03-24 18:27:52 +0800

    interface

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
   <div id="app">
+    <!-- navigation -->
     <nav>
       <span class='menu'></span>
-      <span>我的</span>
-      <span>发现</span>
-      <span>动态</span>
+      <span :class='{focus: currentSlider === 0}' @click='switchSlider($event, 0)'>我的</span>
+      <span :class='{focus: currentSlider === 1}' @click='switchSlider($event, 1)'>发现</span>
+      <span :class='{focus: currentSlider === 2}' @click='switchSlider($event, 2)'>动态</span>
       <span class='search'></span>
     </nav>
-    <!-- <bannerSwipe></bannerSwipe> -->
+    <!-- three components -->
+    <section class='threeSlide'>
+      <mine-music-component :style="{'margin-left': (-100 * currentSlider) + '%'}"></mine-music-component>
+      <discovery-component></discovery-component>
+      <activity-component></activity-component>
+    </section>
+    <!-- play bar -->
+    <play-bar></play-bar>
   </div>
 </template>
 
 <script>
 // import bannerSwipe from './components/bannerSwipe.vue'
-import hideLoader from './tools/hideLoader'
+import playBar from './components/playBar.vue'
+import mineMusicComponent from './components/mineMusic.vue'
+import discoveryComponent from './components/discovery.vue'
+import activityComponent from './components/activity.vue'
 export default {
+  name: 'mainUI',
   data () {
     return {
-      msg: ''
+      currentSlider: 0
     }
   },
   components: {
-    // bannerSwipe
+    playBar,
+    mineMusicComponent,
+    discoveryComponent,
+    activityComponent
   },
   mounted () {
-    hideLoader()
+    // hideLoader()
+  },
+  methods: {
+    switchSlider (e, num) {
+      this.currentSlider = num
+    }
   }
 }
 </script>
@@ -37,24 +57,39 @@ export default {
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    color:#eee;
+    color:#ddd;
     padding: 0 1em;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     > span {
       font-family: 'icomoon' !important;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
+    }
+    > span.focus {
+        color: #fff;
     }
     > span:first-child {
-      font-size: 1.2em;
       &::before {
+        font-size: 1.2em;
         content: "\e9bb"
       }
     }
     > span:last-child {
-      font-size: 1.2em;
       &::before {
+        font-size: 1.2em;
         content: "\e986"
       }
+    }
+  }
+  section.threeSlide {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    > div {
+      width: 100%;
+      height: 100%;
+      flex-shrink: 0;
+      transition: all .3s ease;
     }
   }
 </style>
